c6269270b0be46406f5ccdb91163f84242a0507b
henri <127192852+32henri@users.noreply.github.com>
2023-10-27 10:29:46 +0300

    Add files via upload

---
+++ DatabaseTask1/DatabaseTask.Data/DatabaseTaskDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DatabaseTask.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DatabaseTask.Data
+{
+    public class DatabaseTaskDbContext : DbContext
+    {
+        public DatabaseTaskDbContext(DbContextOptions<DatabaseTaskDbContext> options)
+            : base(options) { }
+
+        // näide, kuidas teha, kui lisate domaini alla ühe objekti
+        // migratsioonid peavad tulema siia libary-sse e TARge20.Data alla.
+
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<ConstructionCompany> ConstructionCompanies { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Specialist> Specialists { get; set; }
+    }
+}
